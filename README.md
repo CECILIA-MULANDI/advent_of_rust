ee4adf52665139ce9f8319ff65b4505c58fff9fe
CECILIA-MULANDI <mulandicecilia4@gmail.com>
2024-12-10 03:56:53 +0300

    day 8

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 # advent_of_rust
-# advent_of_rust
-# advent_of_rust
---
+++ day_8/target/debug/deps/day_8-d84ae53cd8faf870.d
@@ -0,0 +1,5 @@
+/home/cecilia/Developer/advent-of-rust/day_8/target/debug/deps/libday_8-d84ae53cd8faf870.rmeta: src/main.rs
+
+/home/cecilia/Developer/advent-of-rust/day_8/target/debug/deps/day_8-d84ae53cd8faf870.d: src/main.rs
+
+src/main.rs:
