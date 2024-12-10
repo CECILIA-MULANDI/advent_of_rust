a84dd2bad6568b4c8fe4210c3976079e4278a004
CECILIA-MULANDI <mulandicecilia4@gmail.com>
2024-12-10 04:07:18 +0300

    refactoring

--- day_8/target/debug/deps/day_8-e9f7d78525f071ac.d
+++
@@ -1,5 +0,0 @@
-/home/cecilia/Developer/advent-of-rust/day_8/target/debug/deps/libday_8-e9f7d78525f071ac.rmeta: src/main.rs
-
-/home/cecilia/Developer/advent-of-rust/day_8/target/debug/deps/day_8-e9f7d78525f071ac.d: src/main.rs
-
-src/main.rs:
