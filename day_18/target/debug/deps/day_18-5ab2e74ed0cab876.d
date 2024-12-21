c478a26901285a4e540b268e458733b684dc9b97
CECILIA-MULANDI <mulandicecilia4@gmail.com>
2024-12-21 19:16:19 +0300

    a few changes to the code

--- day_18/target/debug/deps/day_18-5ab2e74ed0cab876.d
+++
@@ -1,5 +0,0 @@
-/home/cecilia/Developer/advent-of-rust/day_18/target/debug/deps/libday_18-5ab2e74ed0cab876.rmeta: src/main.rs
-
-/home/cecilia/Developer/advent-of-rust/day_18/target/debug/deps/day_18-5ab2e74ed0cab876.d: src/main.rs
-
-src/main.rs:
