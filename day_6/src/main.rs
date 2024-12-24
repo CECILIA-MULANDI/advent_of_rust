d01238f7e47485dde248b3188e511fcd687e2300
CECILIA-MULANDI <mulandicecilia4@gmail.com>
2024-12-24 19:27:10 +0300

    day_6

---
+++ day_6/src/main.rs
@@ -0,0 +1,14 @@
+// Write a function that returns the reference to the longer string
+// without any new allocations
+pub fn longer_wish<'a>(s1: &'a str, s2: &'a str) -> Option<&'a str> {
+    let s1_len = s1.trim().chars().count();
+    let s2_len = s2.trim().chars().count();
+
+    if s1_len == s2_len {
+        None
+    } else if s1_len > s2_len {
+        Some(s1)
+    } else {
+        Some(s2)
+    }
+}
